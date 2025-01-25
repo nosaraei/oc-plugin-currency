eb42033421bd2b4883d10b2ec7ef01336e9c0a40
Nosaraei <m.nosaraei@gmail.com>
2025-01-25 11:25:35 +0330

    Add currency resource

---
+++ api/resources/Currency.php
@@ -0,0 +1,35 @@
+<?php namespace Responsiv\Currency\Api\Resources;
+
+use System\Traits\Resource;
+
+/**
+ * @api {واحد ارز} / Currency
+ * @apiName Currency
+ * @apiGroup Models
+ *
+ * @apiSuccessExample Currency Fields
+ * {
+ *    "name": "دلار آمریکا",
+ *    "currency_code": "USD",
+ *    "currency_symbol": "$",
+ *    "decimal_point": ".",
+ *    "thousand_separator": ",",
+ *    "place_symbol_before": 1,
+ *    "ratio": 1.25 // or null
+ * }
+ */
+class Currency extends \Responsiv\Currency\Models\Currency {
+
+    use Resource;
+    
+    protected $hidden = [
+        "created_at",
+        "updated_at",
+        "is_enabled",
+        "is_primary",
+        "is_default",
+        "id",
+        "main_currency_id",
+        "to_main_ratio",
+    ];
+}
